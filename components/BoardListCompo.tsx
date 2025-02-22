6821be44a7b714f47235725eb042bfcc75bf992c
jaehe <jaehe0413@naver.com>
2025-02-22 23:15:29 +0900

    feat : Board간 순서 변경 drag & drop

--- components/BoardListCompo.tsx
+++ components/BoardListCompo.tsx
@@ -3,6 +3,7 @@
 import { Board } from "@/lib/boardManager";
 import { BoardCompo } from "./BoardCompo";
 import { Todo, TodoManager } from "@/lib/todoManager";
+import { useState } from "react";
 
 interface BoardListCompo {
   boards: Board[];
@@ -10,6 +11,9 @@ interface BoardListCompo {
 }
 
 export function BoardListCompo({ boards, setBoards }: BoardListCompo) {
+  const [draggingBoard, setDraggingBoard] = useState<Board | null>(null);
+  const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+
   const editBoardName = (boardId: string, name: string) => {
     setBoards(boards.map((b) => (b.id === boardId ? { ...b, name } : b)));
   };
@@ -22,34 +26,100 @@ export function BoardListCompo({ boards, setBoards }: BoardListCompo) {
     setBoards(boards.map((b) => (b.id === board.id ? board : b)));
   };
 
+  const handleDragStart = (board: Board) => {
+    setDraggingBoard(board);
+  };
+
+  const handleDragOver = (index: number) => {
+    if (!draggingBoard) return;
+    setHoverIndex(index);
+  };
+
+  const handleDrop = () => {
+    if (draggingBoard !== null && hoverIndex !== null) {
+      setBoards((prev) => {
+        const newBoards = [...prev];
+        const oldIndex = newBoards.findIndex((b) => b.id === draggingBoard.id);
+        newBoards.splice(oldIndex, 1);
+
+        let newIndex = hoverIndex;
+        if (hoverIndex <= 0) {
+          newIndex = 0;
+        } else if (hoverIndex >= prev.length) {
+          newIndex = prev.length;
+        }
+        newBoards.splice(newIndex, 0, draggingBoard);
+        return newBoards;
+      });
+    }
+    resetDragging();
+  };
+
+  const resetDragging = () => {
+    setDraggingBoard(null);
+    setHoverIndex(null);
+  };
+
+  const handleDragEnd = () => {
+    resetDragging();
+  };
+
   return (
-    <div className="flex gap-4 p-4 overflow-x-auto">
-      {boards.map((board) => (
-        <BoardCompo
+    <div className="flex flex-row p-4 overflow-x-auto">
+      {boards.map((board, index) => (
+        <div
+          className="flex flex-row gap-0"
           key={board.id}
-          board={board}
-          setBoard={setBoard}
-          createTodo={() => {
-            const newTodo: Todo = TodoManager.createTodo();
-            setBoard({ ...board, todos: [...board.todos, newTodo] });
+          draggable
+          onDragStart={() => {
+            handleDragStart(board);
           }}
-          editBoardName={(name) => editBoardName(board.id, name)}
-          deleteBoard={() => deleteBoard(board.id)}
-          changeTodoName={(todoId, name) => {
-            setBoard({
-              ...board,
-              todos: board.todos.map((t) =>
-                t.id === todoId ? { ...t, name } : t
-              ),
-            });
+          onDragOver={(e) => {
+            e.preventDefault();
+            handleDragOver(index);
           }}
-          deleteTodo={(todoId) => {
-            setBoard({
-              ...board,
-              todos: board.todos.filter((t) => t.id !== todoId),
-            });
+          onDrop={() => {
+            handleDrop();
           }}
-        />
+          onDragEnd={handleDragEnd}
+        >
+          <div
+            className={`w-[4px] h-full bg-blue-500 m-auto ${
+              hoverIndex === -1 && index === 0 ? "visible" : "invisible"
+            }`}
+          ></div>
+          <BoardCompo
+            board={board}
+            setBoard={setBoard}
+            createTodo={() => {
+              const newTodo: Todo = TodoManager.createTodo();
+              setBoard({ ...board, todos: [...board.todos, newTodo] });
+            }}
+            editBoardName={(name) => editBoardName(board.id, name)}
+            deleteBoard={() => deleteBoard(board.id)}
+            changeTodoName={(todoId, name) => {
+              setBoard({
+                ...board,
+                todos: board.todos.map((t) =>
+                  t.id === todoId ? { ...t, name } : t
+                ),
+              });
+            }}
+            deleteTodo={(todoId) => {
+              setBoard({
+                ...board,
+                todos: board.todos.filter((t) => t.id !== todoId),
+              });
+            }}
+          />
+          <div className={"w-4 h-full"}>
+            <div
+              className={`w-[4px] h-full bg-blue-500 m-auto ${
+                hoverIndex === index ? "visible" : "invisible"
+              }`}
+            ></div>
+          </div>
+        </div>
       ))}
     </div>
   );
